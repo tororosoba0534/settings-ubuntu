da3e515c105f26102b582e79b7c52db88cc5ad22
tororosoba0534 <tekuteku11udana888@gmail.com>
2022-11-27 13:49:53 +0900

    Succeed in disable custom ctrl-k in the terminal.

--- .config/autokey/data/all/deleteTillEnd.py
+++ .config/autokey/data/all/deleteTillEnd.py
@@ -1,3 +1,10 @@
 #Enter script code
-keyboard.send_keys("<shift>+<end>")
-keyboard.send_keys("<backspace>")
+current_class = window.get_active_class()
+#dialog.info_dialog(message=f'{current_class} is the class name of this window.')
+if current_class == "gnome-terminal-server.Gnome-terminal":
+    #dialog.info_dialog(message="terminal       ")
+    keyboard.send_keys("<ctrl>+k") 
+else
+    keyboard.send_keys("<shift>+<end>")
+    keyboard.send_keys("<backspace>")
+
