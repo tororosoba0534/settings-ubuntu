111a44b18f538a9c27d730059e8367b8f74ac2f8
tororosoba0534 <tekuteku11udana888@gmail.com>
2023-02-15 17:15:38 +0900

    skk環境構築

--- .config/autokey/data/all/backspace.py
+++ .config/autokey/data/all/backspace.py
@@ -1,2 +1,8 @@
 #Enter script
-keyboard.send_keys("<backspace>")
+current_class = window.get_active_class()
+# dialog.info_dialog(message=f'{current_class} is the class name of this window.')
+if current_class in [ "gnome-terminal-server.Gnome-terminal", "Alacritty.Alacritty"]:
+    #dialog.info_dialog(message="terminal       ")
+    keyboard.send_keys("<ctrl>+h")
+else:
+    keyboard.send_keys("<backspace>")
